51a1b0ed42312763acbad7ee3a36f01268601452
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2022-09-15 19:16:26 +0530

    added css functionality

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const dateOfBirth=document.querySelector('#date-of-birth');
 const luckyNumber=document.querySelector('#lucky-number');
 const checkNumberButton=document.querySelector('#check-number');
-checkNumberButton.addEventListener('click', function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
-})
+const outputBox = document.querySelector('#output-box');
+
+function compareValues(sum,luckyNumber){
+if(sum%luckyNumber===0){
+    outputBox.innerText="Your BirthDay is Luckyyyy";
+
+}
+else{
+    outputBox.innerText="your birthday is not lucky";
+}
+}
+
+function checkBirthDateIsLucky(){
+const dob=dateOfBirth.value
+const sum=calculateSum(dob);
+if(sum&&dob){
+compareValues(sum,luckyNumber.value)}
+else{
+    outputBox.innerText="Please enter both the fields";
+}
+}
+function calculateSum(dob){
+  dob=dob.replaceAll("-","");
+  let sum=0;
+  for(let index=0;index<dob.length;index++){
+    sum=sum+ Number(dob.charAt(index));
+  }
+  return sum;
+}
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
